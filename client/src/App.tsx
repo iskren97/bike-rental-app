b1d85aa812f4ad288d414fc1dff307ed04cfa78c
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-24 14:20:39 +0300

    added base components and pages, implemented different base routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './components/Layout';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
