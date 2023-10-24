2a7554746b69943e3d08746371450b9d2297f7d3
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-24 13:10:33 +0300

    added userRouter with endpoints for register, login, get all users

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,14 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import { userRouter } from './routes/users.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/users', userRouter);
 mongoose
   .connect(
     `mongodb+srv://iskrengyorev:${process.env.APP_DB_PASS}@cluster0.8ohdtya.mongodb.net/?retryWrites=true&w=majority`
---
+++ server/src/routes/users.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import UserModel from '../models/Users.js';
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (user) {
+      throw new Error('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+    const newUser = new UserModel({
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const checkPasswordValidity = await bcrypt.compare(password, user.password);
+
+    if (!checkPasswordValidity) {
+      throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.send({ user, token });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+export { router as userRouter };
