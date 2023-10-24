22f05e7d08b8e8c4d566b8663173753532bf031b
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-24 19:49:41 +0300

    added re-usable base button style

---
+++ client/src/styles/styles.ts
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+import { colors } from './constants';
+
+const BaseButton = css`
+  min-height: 3.5rem;
+  padding: 0 1.5rem;
+  cursor: pointer;
+  font-size: 1.5rem;
+  font-weight: bold;
+  transition: all 0.2s;
+  border-radius: 0.5rem;
+  border: solid 1px ${colors.primary};
+`;
+
+export { BaseButton };
