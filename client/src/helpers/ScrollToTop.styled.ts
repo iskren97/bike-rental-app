39292dd7a167bc52efccf3e6561b9f4e6e4638fb
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-25 19:04:27 +0300

    added ScrollToTop helper

---
+++ client/src/helpers/ScrollToTop.styled.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { FaAngleUp } from 'react-icons/fa';
+import { colors } from '../styles/constants';
+
+const ScrollContainer = styled.div`
+  position: relative;
+`;
+
+const ScrollIcon = styled(FaAngleUp)`
+  position: fixed;
+  bottom: 4rem;
+  right: 2.5rem;
+  z-index: 20;
+
+  background-color: ${colors.primary};
+  border: 2px solid ${colors.white};
+  border-radius: 50%;
+  height: 5rem;
+  width: 5rem;
+  color: ${colors.white};
+  cursor: pointer;
+  animation: movingScroll 3s ease-in-out infinite;
+  transition: all 0.5s ease-in-out;
+
+  &:hover {
+    animation: none;
+    background: ${colors.white};
+    color: ${colors.primary};
+    border: 2px solid ${colors.primary};
+  }
+
+  @keyframes movingScroll {
+    0% {
+      transform: translateY(0px);
+    }
+    25% {
+      transform: translateY(20px);
+    }
+    50% {
+      transform: translateY(0px);
+    }
+    75% {
+      transform: translateY(-20px);
+    }
+    100% {
+      transform: translateY(0px);
+    }
+  }
+`;
+
+export { ScrollContainer, ScrollIcon };
---
+++ client/src/helpers/ScrollToTop.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { ScrollContainer, ScrollIcon } from './ScrollToTop.styled';
+import { animateScroll as scroll } from 'react-scroll';
+
+const ScrollToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
+  const goToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <ScrollContainer>
+      {showTopBtn && <ScrollIcon onClick={goToTop} />}
+    </ScrollContainer>
+  );
+};
+export default ScrollToTop;
