de81d900656664aa237e5dd59452a27332d7272b
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-25 15:53:42 +0300

    minor design imporvement on BookingInfo component

--- client/src/components/BookingInfo/BookingInfo.styled.ts
+++ client/src/components/BookingInfo/BookingInfo.styled.ts
@@ -2,14 +2,18 @@ import styled from 'styled-components';
 
 const BookingInfoContainer = styled.div`
   display: flex;
-  alignitems: center;
+  align-items: center;
   justify-content: center;
   gap: 10rem;
   text-align: center;
   padding: 10rem 0;
 `;
 
-const InnerContainer = styled.div``;
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 const InfoHeading = styled.h2`
   font-size: 3rem;
