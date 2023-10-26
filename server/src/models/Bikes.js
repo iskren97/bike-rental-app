9e34fe47de1bcbd44db500a253ca325f970e331c
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-26 12:48:12 +0300

    changed bikesSchema property types

--- server/src/models/Bikes.js
+++ server/src/models/Bikes.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose';
 const bikesSchema = new mongoose.Schema({
   model: { type: String, required: true },
   color: { type: String, required: true },
-  location: { type: Number, required: true },
-  rating: { type: String, required: true },
+  location: { type: String, required: true },
+  rating: { type: Number, required: true },
   availability: { type: Boolean, required: true },
   reservedBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'users',
-    required: true,
+    default: null,
   },
 });
 
