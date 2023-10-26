53a90d50464bb8102d1db12670ce11664915b9b3
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-26 15:14:47 +0300

    added additional dummy info to AboutSection

--- client/src/components/AboutSection/AboutSection.tsx
+++ client/src/components/AboutSection/AboutSection.tsx
@@ -11,11 +11,20 @@ const AboutSection = () => {
   return (
     <AboutSectionContainer id="about">
       <AboutBikeImage src={AboutBike} alt="bike" />
-
       <AboutDescriptionContainer>
         <AboutHeading>
           Explore our great deals and choose the one that suits you the most
         </AboutHeading>
+        <AboutDescParagraph>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
+          eligendi consectetur totam illo doloremque quo nostrum facere, facilis
+          ipsa fugit, vitae suscipit. Itaque temporibus dolores alias debitis
+          esse pariatur doloremque! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Qui autem ratione in repellendus dolorem
+          consequuntur fugiat laboriosam et eos quam veniam doloremque
+          reiciendis dolorum quisquam magni, cupiditate eum sint excepturi.
+        </AboutDescParagraph>
+
         <AboutDescParagraph>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
           eligendi consectetur totam illo doloremque quo nostrum facere, facilis
